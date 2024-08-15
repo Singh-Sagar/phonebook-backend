0dc11fb86b04e63eb1e38bfa9a6a94b130c213bf
Singh-Sagar <arcsagar.91@gmail.com>
2024-08-15 11:34:36 +0530

    mongodb basic configuration done

--- mongo.js
+++ mongo.js
@@ -5,10 +5,12 @@ if (process.argv.length<3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://arcsagar91:${password}@cluster0.3beansg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -16,17 +18,27 @@ mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   name: String,
-  number: String
+  number: String,
 })
 
 const Note = mongoose.model('Note', noteSchema)
+if(process.argv.length === 5){
+    const note = new Note({
+        name,
+        number
+      })
 
-const note = new Note({
-  name: 'HTML is easy',
-  number: '3923-32039',
-})
+    note.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+    })
+}else if(process.argv.length<=3){
+    Note.find({}).then(result => {
+        result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close()
+      })
+}
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+
